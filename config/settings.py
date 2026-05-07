b058736f9a4f2229177be9fa9a7114a0eba861aa
Никита Станьков <9alsara9@gmail.com>
2026-05-07 23:25:24 +0300

    .

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tracker',
     'drf_yasg',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- tracker/models.py
+++ tracker/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 
 class Worker(models.Model):
-    name = models.TextField(blank=True, null=True)
-    state = models.TextField(blank=True, null=True)
+    name = models.CharField(blank=True, null=True)
+    role = models.CharField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.name} ({self.state})"
+        return f"{self.name} ({self.role})"
 
 class Task(models.Model):
-    name = models.TextField(blank=True, null=True)
+    class Status:
+        ACTIVE = "ACTIVE"
+        DONE = "DONE"
+        WAITING = "WAITING"
+
+    STATUS_CHOICES = [
+        (Status.ACTIVE, "Active"),
+        (Status.DONE, "Done"),
+        (Status.WAITING, "Waiting"),
+    ]
+    name = models.CharField(blank=True, null=True)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='children')
     worker = models.ForeignKey(Worker, related_name='tasks', on_delete=models.CASCADE)
     deadline = models.DateTimeField(blank=True, null=True)
-    state = models.TextField(blank=True, null=True)
+    state = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default=Status.WAITING
+    )
 
     def __str__(self):
         return f"{self.name} ({self.state})"
